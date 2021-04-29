9289a84c32fbf4e5b9f8bf3fff7c1798bf03649f
Giorgio Giuffrè <giorgiogiuffre23@gmail.com>
2021-04-29 22:11:25 +0200

    Use Firestore instead of local state for events index page

--- lib/data/event.dart
+++ lib/data/event.dart
@@ -1,13 +1,19 @@
 import 'dart:ui';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Event implements Comparable {
   final String id;
   final String title;
   final DateTime time;
 
-  Event(this.title, time)
+  Event(this.title, DateTime time)
       : time = DateTime(time.year, time.month, time.day),
         id = "$time$title";
+
+  Event.fromFirestore(this.title, Timestamp timestamp)
+      : time = timestamp.toDate(),
+        id = "${timestamp.toDate()}$title";
 
   @override
   int compareTo(other) => time == other.time ? title.compareTo(other.title) : time.compareTo(other.time);
--- lib/ui/components/events_index.dart
+++ lib/ui/components/events_index.dart
@@ -1,32 +1,52 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../data/date_formatting.dart';
 import '../../data/event.dart';
-import '../../data/event_provider.dart';
 
 class EventsIndex extends ConsumerWidget {
   final void Function(Event) onEventTapped;
+  final _user = FirebaseAuth.instance.currentUser;
 
-  const EventsIndex({Key? key, required this.onEventTapped}) : super(key: key);
+  EventsIndex({Key? key, required this.onEventTapped}) : super(key: key);
 
   @override
   Widget build(context, watch) {
-    final events = watch(eventProvider).reversed.toList();
-    return events.isEmpty
-        ? const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Center(child: Text("No events yet")),
-          )
-        : ListView.separated(
-            padding: const EdgeInsets.all(8.0),
-            itemCount: events.length,
-            separatorBuilder: (BuildContext context, int index) => const Divider(),
-            itemBuilder: (BuildContext context, int index) => ListTile(
-              title: Text(events[index].title),
-              onTap: () => onEventTapped(events[index]),
-              trailing: Text(formattedDate(events[index].time)),
-            ),
-          );
+    // final events = watch(eventProvider).reversed.toList();
+    return FutureBuilder<Iterable<Event>>(
+        future: FirebaseFirestore.instance
+            .collection("users")
+            .doc(_user?.uid)
+            .collection("events")
+            .get()
+            .then((collection) => collection.docs.map((e) => Event.fromFirestore(e.data()["title"], e.data()["time"]))),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(child: Text("Couldn't retrieve events."));
+          }
+
+          if (snapshot.connectionState == ConnectionState.done) {
+            final events = snapshot.data?.toList() ?? [];
+            return events.isEmpty
+                ? const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Center(child: Text("No events yet")),
+                  )
+                : ListView.separated(
+                    padding: const EdgeInsets.all(8.0),
+                    itemCount: events.length,
+                    separatorBuilder: (BuildContext context, int index) => const Divider(),
+                    itemBuilder: (BuildContext context, int index) => ListTile(
+                      title: Text(events[index].title),
+                      onTap: () => onEventTapped(events[index]),
+                      trailing: Text(formattedDate(events[index].time)),
+                    ),
+                  );
+          }
+
+          return const Center(child: CircularProgressIndicator.adaptive());
+        });
   }
 }
