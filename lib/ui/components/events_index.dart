0ba33fa5ae78e567f0a2af6ff91313b00729ff0d
Giorgio Giuffrè <giorgiogiuffre23@gmail.com>
2021-04-30 18:58:43 +0200

    Format code

--- lib/ui/components/events_index.dart
+++ lib/ui/components/events_index.dart
@@ -16,37 +16,38 @@ class EventsIndex extends ConsumerWidget {
   Widget build(context, watch) {
     // final events = watch(eventProvider).reversed.toList();
     return FutureBuilder<Iterable<Event>>(
-        future: FirebaseFirestore.instance
-            .collection("users")
-            .doc(_user?.uid)
-            .collection("events")
-            .get()
-            .then((collection) => collection.docs.map((e) => Event.fromFirestore(e.data()["title"], e.data()["time"]))),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return const Center(child: Text("Couldn't retrieve events."));
-          }
+      future: FirebaseFirestore.instance
+          .collection("users")
+          .doc(_user?.uid)
+          .collection("events")
+          .get()
+          .then((collection) => collection.docs.map((e) => Event.fromFirestore(e.data()["title"], e.data()["time"]))),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const Center(child: Text("Couldn't retrieve events."));
+        }
 
-          if (snapshot.connectionState == ConnectionState.done) {
-            final events = snapshot.data?.toList() ?? [];
-            return events.isEmpty
-                ? const Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Center(child: Text("No events yet")),
-                  )
-                : ListView.separated(
-                    padding: const EdgeInsets.all(8.0),
-                    itemCount: events.length,
-                    separatorBuilder: (BuildContext context, int index) => const Divider(),
-                    itemBuilder: (BuildContext context, int index) => ListTile(
-                      title: Text(events[index].title),
-                      onTap: () => onEventTapped(events[index]),
-                      trailing: Text(formattedDate(events[index].time)),
-                    ),
-                  );
-          }
+        if (snapshot.connectionState == ConnectionState.done) {
+          final events = snapshot.data?.toList() ?? [];
+          return events.isEmpty
+              ? const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Center(child: Text("No events yet")),
+                )
+              : ListView.separated(
+                  padding: const EdgeInsets.all(8.0),
+                  itemCount: events.length,
+                  separatorBuilder: (BuildContext context, int index) => const Divider(),
+                  itemBuilder: (BuildContext context, int index) => ListTile(
+                    title: Text(events[index].title),
+                    onTap: () => onEventTapped(events[index]),
+                    trailing: Text(formattedDate(events[index].time)),
+                  ),
+                );
+        }
 
-          return const Center(child: CircularProgressIndicator.adaptive());
-        });
+        return const Center(child: CircularProgressIndicator.adaptive());
+      },
+    );
   }
 }
