e5b8916184dcf7eaf6e896cb3cb34298a2e2f6e9
ggiuffre <ggiuffre@users.noreply.github.com>
2021-05-23 16:11:52 +0000

    deploy: 86797671bcf998cf4665563a970084df4e0c0858

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,17 +5,17 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "658197070290b8a9f7a2d1cdcb6378f9",
 "main.dart.js": "dbd3703a77c0b7698e8ad063e59ab91e",
-"index.html": "9240ab413c3ac129c28ee2d36b0091ae",
-"/": "9240ab413c3ac129c28ee2d36b0091ae",
+"index.html": "678c5024c9cc8d07f75ad3b0934a4341",
+"/": "678c5024c9cc8d07f75ad3b0934a4341",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/NOTICES": "f08022adf04ece3c5ce60fda3b8dbacd",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "b2e2d2d60e36e276bf7a3ca6d0ba6d8e",
+"icons/Icon-192.png": "358a83a3a25c1e3da264972b63fcc47a",
 "manifest.json": "9285a43f7d9a0c7cfc1061be057060f5",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796"
+"favicon.png": "9407e3a0ae85a697c4e9464546a20e33"
 };
 
 // The application shell files that are downloaded before a service worker can
