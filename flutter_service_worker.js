eb25e555c4323ba7697bd73e07f54b1518dc3907
ggiuffre <ggiuffre@users.noreply.github.com>
2021-06-05 19:55:50 +0000

    deploy: 07e74d3f37cd9fc0862eda43dedbcbd9f42ccfa9

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "658197070290b8a9f7a2d1cdcb6378f9",
-"main.dart.js": "973c2f6849d4465b30abbc98088d88e9",
-"index.html": "0ef3d4d9f799a6016c6b44a24e5882fb",
-"/": "0ef3d4d9f799a6016c6b44a24e5882fb",
+"main.dart.js": "6c9721ce8b5201eedc0ab698d69cfbf7",
+"index.html": "5b79a78786a2a420ae9cfef3d4b2607e",
+"/": "5b79a78786a2a420ae9cfef3d4b2607e",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/NOTICES": "8395f74153dafefd689dad985c8bc027",
