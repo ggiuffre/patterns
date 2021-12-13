1aceb6d88e25e0d9c323c2d1df26b2bda23f63ce
ggiuffre <ggiuffre@users.noreply.github.com>
2021-12-13 09:04:11 +0000

    deploy: 9fd2b4d86baaa275f106df52c52d0780b70598d5

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -7,19 +7,19 @@ const RESOURCES = {
 "canvaskit/profiling/canvaskit.js": "f3bfccc993a1e0bfdd3440af60d99df4",
 "canvaskit/profiling/canvaskit.wasm": "a9610cf39260f60fbe7524a785c66101",
 "canvaskit/canvaskit.wasm": "04ed3c745ff1dee16504be01f9623498",
+"main.dart.js": "6e8002397cd54cbee23f5d44988cadea",
 "version.json": "76b2fe1715647e97b513252bb6fc6e1c",
 "manifest.json": "9285a43f7d9a0c7cfc1061be057060f5",
 "icons/Icon-512.png": "8cc827572d48a738a7f845d74c00a549",
 "icons/Icon-192.png": "5be85357c159250bf456345a1e175cc0",
-"main.dart.js": "49e8cb5f01779a0a01301a859c40145a",
-"favicon.png": "c29c6336e5b9ae82247c88f957d52147",
-"index.html": "cd873727680c632b360f35fb54c65e23",
-"/": "cd873727680c632b360f35fb54c65e23",
+"assets/NOTICES": "05ed957c57125670a15cd5daa894e008",
+"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/NOTICES": "05ed957c57125670a15cd5daa894e008",
-"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e"
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
+"index.html": "523a60fb1b37694562385ad197ee9697",
+"/": "523a60fb1b37694562385ad197ee9697",
+"favicon.png": "c29c6336e5b9ae82247c88f957d52147"
 };
 
 // The application shell files that are downloaded before a service worker can
