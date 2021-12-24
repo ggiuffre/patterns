e82f5aba8bd431c0c8ea201366bb6f90e4b345e0
Giorgio Giuffrè <giorgiogiuffre23@gmail.com>
2021-12-24 13:12:15 +0100

    Add sports, add function to compute calories from sport+weight+duration

--- lib/src/data/sports_info.dart
+++ lib/src/data/sports_info.dart
@@ -14,18 +14,42 @@ class SportsInfo {
 
 enum Sport {
   running,
+  cycling,
   muscleWorkout,
   alpineSki,
   crossCountrySki,
   swimming,
+  tableTennis,
   other,
 }
 
 const sportsInfo = {
   Sport.running: SportsInfo(sport: "running", calories: 900),
+  Sport.cycling: SportsInfo(sport: "cycling", calories: 640),
   Sport.muscleWorkout: SportsInfo(sport: "muscle workout", calories: 250),
   Sport.alpineSki: SportsInfo(sport: "alpine skiing", calories: 490),
   Sport.crossCountrySki: SportsInfo(sport: "cross-country skiing", calories: 655),
   Sport.swimming: SportsInfo(sport: "swimming", calories: 570),
+  Sport.tableTennis: SportsInfo(sport: "ping pong", calories: 320),
   Sport.other: SportsInfo(sport: "other"),
 };
+
+const caloriesBurntPerKilogramPerHourBySport = {
+  Sport.running: 12.5,
+  Sport.cycling: 8.0,
+  Sport.muscleWorkout: 3.0,
+  Sport.alpineSki: 6.0,
+  Sport.crossCountrySki: 8.0,
+  Sport.swimming: 6.5,
+  Sport.tableTennis: 4.0,
+  Sport.other: 7.0,
+};
+
+double calorieConsumption({required Sport sport, required double bodyWeight, double hours = 1.0}) {
+  final coefficient = caloriesBurntPerKilogramPerHourBySport[sport];
+  if (coefficient != null) {
+    return coefficient * bodyWeight * hours;
+  } else {
+    return 7.0 * bodyWeight * hours;
+  }
+}
