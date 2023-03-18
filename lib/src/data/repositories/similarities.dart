87d1fa8c3541202264f51cff58361b0c39d2652c
Giorgio Giuffrè <giorgiogiuffre23@gmail.com>
2023-03-18 09:23:37 +0100

    Structure SimilarityRepository as cache interface
    This will allow to seamlessly compute similarities ina  way that is more
    scalable than the current solution.

--- lib/src/data/repositories/similarities.dart
+++ lib/src/data/repositories/similarities.dart
@@ -1,15 +1,57 @@
 import 'package:cloud_firestore/cloud_firestore.dart' show FirebaseFirestore;
 import 'package:firebase_auth/firebase_auth.dart' show FirebaseAuth;
 
+import '../event.dart';
 import '../similarities.dart';
 
-class SimilarityRepository {
+/// A repository of [Similarity] objects.
+abstract class SimilarityRepository {
+  /// Get the similarity between two event iterables.
+  Future<Similarity> get(Iterable<Event> aEvents, Iterable<Event> bEvents);
+}
+
+/// Implementation of [SimilarityRepository] with a Firestore back-end.
+///
+/// Similarities are only read from (and persisted to) Cloud Firestore if the
+/// app user is currently logged via Firebase. Otherwise all methods of this
+/// class return a future that resolves in an error or a future that emits an
+/// error.
+class FirestoreSimilarityRepository implements SimilarityRepository {
   final _userId = FirebaseAuth.instance.currentUser?.uid;
 
-  /// Save a similarity to Cloud Firestore.
-  Future<void> add(Similarity similarity) async {
+  /// Get the similarity between two event iterables, retrieving it from from
+  /// Cloud Firestore if cached.
+  @override
+  Future<Similarity> get(Iterable<Event> aEvents, Iterable<Event> bEvents,
+      {bool binary = false}) async {
     if (_userId == null) {
-      return Future.error("Couldn't persist event to Cloud Firestore.");
+      return Future.error("Couldn't retrieve similarity from Cloud Firestore.");
+    }
+
+    return FirebaseFirestore.instance
+        .collection("users")
+        .doc(_userId)
+        .collection("similarities")
+        .doc([aEvents, bEvents].hashCode.toString())
+        .get()
+        .then(
+            (d) => Similarity(
+                  {aEvents.first.title, bEvents.first.title},
+                  double.tryParse(d.data()?["value"] ?? "0") ?? 0,
+                ), onError: (_) {
+      final sim = Similarity(
+        {aEvents.first.title, bEvents.first.title},
+        similarity(aEvents, bEvents, binary: binary),
+      );
+      _add([aEvents, bEvents].hashCode.toString(), sim.coefficient);
+      return sim;
+    });
+  }
+
+  /// Save a similarity to Cloud Firestore.
+  Future<void> _add(String eventsHash, double coefficient) async {
+    if (_userId == null) {
+      return Future.error("Couldn't persist similarity to Cloud Firestore.");
     }
 
     FirebaseFirestore.instance
@@ -17,22 +59,8 @@ class SimilarityRepository {
         .doc(_userId)
         .collection("similarities")
         .add({
-      "labelsHash": similarity.labels.hashCode.toString(),
-      "value": similarity.coefficient.toString(),
+      "eventsHash": eventsHash,
+      "value": coefficient,
     });
   }
-
-  /// Get a similarity from Cloud Firestore, given its [labels].
-  Future<Similarity> get(Set<String> labels) => _userId == null
-      ? Future.error("Couldn't retrieve similarity from Cloud Firestore.")
-      : FirebaseFirestore.instance
-          .collection("users")
-          .doc(_userId)
-          .collection("similarities")
-          .doc(labels.hashCode.toString())
-          .get()
-          .then((e) => Similarity(
-                labels,
-                double.tryParse(e.data()?["value"] ?? "0") ?? 0,
-              ));
 }
