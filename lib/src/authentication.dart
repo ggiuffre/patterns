72b7a6070bb0070ba41196bbfdd17e3a2e0ee108
Giorgio Giuffrè <giorgiogiuffre23@gmail.com>
2023-04-01 10:02:47 +0200

    Remove redundant MaterialApp inside MaterialApp

--- lib/src/authentication.dart
+++ lib/src/authentication.dart
@@ -1,35 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'data/app_settings_provider.dart';
-import 'theme.dart';
 import 'ui/components/custom_app_bar.dart';
 import 'ui/components/login.dart';
 import 'ui/components/signup.dart';
 
-class SignUpLogInSelector extends ConsumerWidget {
+class SignUpLogInSelector extends StatelessWidget {
   const SignUpLogInSelector({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) => MaterialApp(
-        theme: AppTheme.light,
-        darkTheme: AppTheme.dark,
-        themeMode: ref.read(appSettingsProvider).themeMode,
-        title: 'Patterns',
-        home: Scaffold(
-          appBar: CustomAppBar(title: const Text("Authenticate")),
-          body: ListView(
-            children: [
-              const SignUpScreen(),
-              Center(
-                child: ConstrainedBox(
-                  constraints: const BoxConstraints(maxWidth: 680.0),
-                  child: const _OrText(),
-                ),
+  Widget build(BuildContext context) => Scaffold(
+        appBar: CustomAppBar(title: const Text("Authenticate")),
+        body: ListView(
+          children: [
+            const SignUpScreen(),
+            Center(
+              child: ConstrainedBox(
+                constraints: const BoxConstraints(maxWidth: 680.0),
+                child: const _OrText(),
               ),
-              const LogInScreen(),
-            ],
-          ),
+            ),
+            const LogInScreen(),
+          ],
         ),
       );
 }
