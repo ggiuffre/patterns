d8d072fec30ff188f3b60136d0bbf9b082d70ffd
Giorgio Giuffrè <giorgiogiuffre23@gmail.com>
2023-04-29 19:31:06 +0200

    Simplify provider of similarities

---
+++ lib/src/data/repositories/similarity_providers.dart
@@ -0,0 +1,127 @@
+import 'dart:convert' show utf8;
+import 'dart:developer' as developer;
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:crypto/crypto.dart' show sha1;
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../category.dart';
+import '../event.dart';
+import '../similarities.dart';
+import 'event_providers.dart';
+
+final similarityList = FutureProvider<Iterable<Similarity>>((ref) async {
+  final events = ref.watch(eventList).value ?? [];
+  const binary = false; // TODO compute similarities between binary events
+
+  Map<String, List<Event>> eventsByCategory = {};
+  for (final event in _batchEvents(events).first) {
+    eventsByCategory.putIfAbsent(event.title, () => []).add(event);
+  }
+
+  final cachedSimilarities = await _fetchAll();
+
+  List<Similarity> coefficients = [];
+  Set visitedCategories = {};
+  final categories = eventsByCategory.keys.toSet();
+  for (final a in categories) {
+    for (final b in categories) {
+      if (a != b &&
+          !visitedCategories.contains(b) &&
+          eventsByCategory.containsKey(a) &&
+          eventsByCategory.containsKey(b)) {
+        final aEvents = eventsByCategory[a]!..sort();
+        final bEvents = eventsByCategory[b]!..sort();
+        final similarityHash = _eventPairHash(aEvents, bEvents);
+        if (cachedSimilarities.containsKey(similarityHash) &&
+            cachedSimilarities[similarityHash] != null) {
+          coefficients.add(
+            Similarity(
+              {aEvents.first.title, bEvents.first.title},
+              cachedSimilarities[similarityHash]!,
+            ),
+          );
+        } else {
+          final similarityHash = _eventPairHash(aEvents, bEvents);
+          final similarityCoefficient =
+              similarity(aEvents, bEvents, binary: binary);
+          await _add(similarityHash, similarityCoefficient);
+          coefficients.add(
+            Similarity({aEvents.first.title, bEvents.first.title},
+                similarityCoefficient),
+          );
+        }
+      }
+    }
+    visitedCategories.add(a);
+  }
+
+  return coefficients..sort((a, b) => a.coefficient.compareTo(b.coefficient));
+});
+
+/// Generate batches of events ordered from most to least relevant, taken from
+/// the current event provider. Relevance is event title frequency, and each
+/// element of the iterable generated is a batch of at most [batchSize] events.
+Iterable<Iterable<Event>> _batchEvents(final Iterable<Event> events,
+    [final int batchSize = 10]) sync* {
+  final categories = categoriesFromEvents(events);
+  final categoryBatch = <String>{};
+  int i = 0;
+  while (i < categories.length && categories.elementAt(i).count > 1) {
+    categoryBatch.add(categories.elementAt(i).title);
+    if (categoryBatch.length >= batchSize) {
+      developer.log("Returning a batch of events.", name: "similarityList");
+      yield events.where((e) => categoryBatch.contains(e.title));
+      categoryBatch.clear();
+    }
+    i++;
+  }
+}
+
+/// Retrieve all similarities stored on Cloud Firestore.
+Future<Map<String, double>> _fetchAll() async {
+  final userId = FirebaseAuth.instance.currentUser?.uid;
+
+  if (userId == null) {
+    return Future.error("Couldn't retrieve similarity from Cloud Firestore.");
+  }
+
+  return FirebaseFirestore.instance
+      .collection("users")
+      .doc(userId)
+      .collection("similarities")
+      .snapshots()
+      .first
+      .then((snapshot) => Map.fromEntries(snapshot.docs.map((e) => MapEntry(
+            e.id,
+            e.data()["value"] ?? 0,
+          ))));
+}
+
+/// Get the SHA1 hash of two lists of events, as a string.
+String _eventPairHash(List<Event> aEvents, List<Event> bEvents) => sha1
+    .convert(utf8.encode(
+      aEvents.map((e) => e.hashCode).join() +
+          bEvents.map((e) => e.hashCode).join(),
+    ))
+    .toString();
+
+/// Save a similarity to Cloud Firestore.
+Future<void> _add(String similarityHash, double similarityCoefficient) async {
+  final userId = FirebaseAuth.instance.currentUser?.uid;
+
+  if (userId == null) {
+    return Future.error("Couldn't persist similarity to Cloud Firestore.");
+  }
+
+  developer.log("Saving new similarity to Cloud Firestore.",
+      name: "similarityList");
+
+  await FirebaseFirestore.instance
+      .collection("users")
+      .doc(userId)
+      .collection("similarities")
+      .doc(similarityHash)
+      .set({"value": similarityCoefficient});
+}
--- lib/src/ui/components/patterns_index.dart
+++ lib/src/ui/components/patterns_index.dart
@@ -3,8 +3,7 @@ import 'dart:developer' as developer;
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../data/repositories/similarities.dart';
-import '../../data/similarities.dart';
+import '../../data/repositories/similarity_providers.dart';
 import 'error_card.dart';
 
 class PatternsIndex extends ConsumerWidget {
@@ -12,7 +11,7 @@ class PatternsIndex extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return ref.watch(similarityProvider).when(
+    return ref.watch(similarityList).when(
           loading: () =>
               const Center(child: CircularProgressIndicator.adaptive()),
           error: (error, stackTrace) {
@@ -20,47 +19,34 @@ class PatternsIndex extends ConsumerWidget {
               "Couldn't retrieve events needed to extract patterns.",
               error: error,
               stackTrace: stackTrace,
+              name: "PatternsIndex",
             );
             return const ErrorCard(
                 text: "Couldn't retrieve events needed to extract patterns.");
           },
-          data: (repo) => FutureBuilder<List<Similarity>>(
-            future: repo.list(),
-            builder: (context, snapshot) {
-              if (snapshot.hasError) {
-                developer.log(snapshot.error.toString());
-                return const ErrorCard(
-                    text: "Couldn't retrieve patterns from events.");
-              }
-
-              if (snapshot.hasData) {
-                final coefficients = snapshot.data ?? [];
-                return coefficients.isEmpty
-                    ? const Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Center(child: Text("No patterns yet")),
-                      )
-                    : ListView.separated(
-                        padding: const EdgeInsets.all(8.0),
-                        itemCount: coefficients.length,
-                        separatorBuilder: (BuildContext context, int index) =>
-                            const Divider(),
-                        itemBuilder: (BuildContext context, int index) =>
-                            ListTile(
-                          title: Text(
-                              "${coefficients[index].labels.first} && ${coefficients[index].labels.last}"),
-                          onTap: () =>
-                              developer.log("tapped ${coefficients[index]}"),
-                          trailing: Text(coefficients[index]
-                              .coefficient
-                              .toStringAsPrecision(3)),
-                        ),
-                      );
-              }
-
-              return const Center(child: CircularProgressIndicator.adaptive());
-            },
-          ),
+          data: (data) {
+            final coefficients = data.toList();
+            return coefficients.isEmpty
+                ? const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Center(child: Text("No patterns yet")),
+                  )
+                : ListView.separated(
+                    padding: const EdgeInsets.all(8.0),
+                    itemCount: coefficients.length,
+                    separatorBuilder: (BuildContext context, int index) =>
+                        const Divider(),
+                    itemBuilder: (BuildContext context, int index) => ListTile(
+                      title: Text(
+                          "${coefficients[index].labels.first} && ${coefficients[index].labels.last}"),
+                      onTap: () =>
+                          developer.log("tapped ${coefficients[index]}"),
+                      trailing: Text(coefficients[index]
+                          .coefficient
+                          .toStringAsPrecision(3)),
+                    ),
+                  );
+          },
         );
   }
 }
