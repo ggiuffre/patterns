6c3987314658816dcde07a756367b0078fe5b894
Giorgio Giuffrè <giorgiogiuffre23@gmail.com>
2023-05-13 20:04:19 +0200

    Improve source code and tests of TF-IDF logic

--- lib/src/data/repositories/stats.dart
+++ lib/src/data/repositories/stats.dart
@@ -1,37 +1,67 @@
+import 'dart:math' show log;
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../event.dart';
+import '../event.dart' show Event;
 import 'event_providers.dart';
 
+typedef EventTitle = String;
+typedef Word = String;
+
+final punctuationMatcher = RegExp(r"(\s+)|[!._\-,'@?/]");
+
 /// Get a map that assigns term frequencies to each event title. A term
-/// frequency is itself a map that assigns a count to each word in a title.
-Map<String, Map<String, int>> termFrequencies(Iterable<Event> events) {
-  final titles = events.map((e) => e.title);
-  final termFrequenciesByEvent = <String, Map<String, int>>{};
+/// frequency is itself a map that assigns a frequency to each word in a title.
+Map<EventTitle, Map<Word, double>> termFrequencies(Iterable<Event> events,
+    {bool caseSensitive = false}) {
+  final titles =
+      events.map((e) => caseSensitive ? e.title : e.title.toLowerCase());
+  final termFrequenciesByEventTitle = <EventTitle, Map<Word, double>>{};
   for (final title in titles) {
-    final histogram = <String, int>{};
-    for (final word in title.split(' ')) {
-      histogram[word] = (histogram[word] ?? 0) + 1;
+    final wordCounts = <Word, int>{};
+    final words = title.split(punctuationMatcher).where((e) => e != '');
+    for (final word in words) {
+      wordCounts[word] = (wordCounts[word] ?? 0) + 1;
     }
-    termFrequenciesByEvent[title] = histogram;
+    termFrequenciesByEventTitle[title] =
+        wordCounts.map((key, value) => MapEntry(key, value / words.length));
   }
-  return termFrequenciesByEvent;
+  return termFrequenciesByEventTitle;
 }
 
-/// Get how often a word appears in a set of event titles.
-Map<String, int> documentFrequencies(Iterable<Event> events) {
-  final wordLists = events.map((e) => e.title.split(' '));
-  final words = wordLists.expand((i) => i).toList();
-  final frequencies = <String, int>{};
-  for (final word in words) {
-    frequencies[word] = (frequencies[word] ?? 0) + 1;
-  }
-  return frequencies;
+/// Get how much information a word conveys across a corpus of event titles.
+Map<Word, double> inverseDocumentFrequencies(Iterable<Event> events,
+    {bool caseSensitive = false}) {
+  final titles =
+      events.map((e) => caseSensitive ? e.title : e.title.toLowerCase());
+  final wordLists = events
+      .map((e) => e.title.split(punctuationMatcher).where((e) => e != ''));
+  final words = wordLists.expand((i) => i).toSet();
+  final wordPresence = {
+    for (final word in words) word: _numDocumentsContainingWord(word, titles)
+  };
+  final inverseFrequencies = wordPresence
+      .map((key, value) => MapEntry(key, log(events.length / value)));
+  return inverseFrequencies;
 }
+
+/// Get how many strings contain a certain word, given an iterable of strings.
+int _numDocumentsContainingWord(Word word, Iterable<String> documents) =>
+    documents.fold(
+      0,
+      (count, doc) =>
+          doc.split(punctuationMatcher).where((e) => e != '').contains(word)
+              ? count + 1
+              : count,
+    );
 
 /// TF-IDF, term frequency * inverse document frequency.
-final tfidf = FutureProvider<Map<String, int>>((ref) {
+final tfidf = FutureProvider<Map<EventTitle, Map<Word, double>>>((ref) {
   final events = ref.watch(eventList).value ?? [];
-  return termFrequencies(events).map(
-      (key, value) => MapEntry(key, documentFrequencies(events)[key] ?? 0));
+  final tf = termFrequencies(events);
+  final idf = inverseDocumentFrequencies(events);
+  return tf.map((eventTitle, value) => MapEntry(
+        eventTitle,
+        value.map((word, value) => MapEntry(word, value * (idf[word] ?? 0.0))),
+      ));
 });
--- test/stats_test.dart
+++ test/stats_test.dart
@@ -12,25 +12,132 @@ void main() {
       expect(result, isEmpty);
     });
 
-    test("of a singleton with a one-word title has 1 value", () {
-      final events = [randomEvent(title: "test")];
+    test("of a singleton has 1 entry", () {
+      final events = [randomEvent()];
       final result = termFrequencies(events);
       expect(result.length, 1);
     });
 
-    test("of a singleton whose title is the repetition of 1 word has 1 value",
+    test("of a singleton with a one-word title has 1 value with 1 entry", () {
+      final events = [randomEvent(title: "test")];
+      final result = termFrequencies(events);
+      expect(result.values.first.length, 1);
+    });
+
+    test("of a singleton with a known title has 1 entry at a known key", () {
+      final event = randomEvent();
+      final events = [event];
+      final result = termFrequencies(events);
+      expect(result[event.title], isNotNull);
+    });
+
+    test(
+        "of a singleton with a one-word title has 1 value with 1 entry at a known key",
+        () {
+      const title = "test";
+      final events = [randomEvent(title: title)];
+      final result = termFrequencies(events);
+      expect(result[title]?[title], isNotNull);
+    });
+
+    test("of a singleton whose title is the repetition of 1 word has 1 entry",
         () {
       final title = List.generate(randomInt(), (index) => "test").join(' ');
       final events = [randomEvent(title: title)];
       final result = termFrequencies(events);
       expect(result.length, 1);
     });
+
+    test("of 2 events with the same title has 1 entry", () {
+      const title = "test hello";
+      final events = [randomEvent(title: title), randomEvent(title: title)];
+      final result = termFrequencies(events);
+      expect(result.length, 1);
+    });
+
+    test("of 2 events with distinct titles has 2 entry", () {
+      final events = [
+        randomEvent(title: "hello abc"),
+        randomEvent(title: "test"),
+      ];
+      final result = termFrequencies(events);
+      expect(result.length, 2);
+    });
+
+    test("of of Florida Man headlines have known scores", () {
+      const titles = [
+        "Florida man Breaks into Joes Crab Shack, Steals Alcohol, Leaves Poop as payment",
+        "Florida Man Pretending to Be Cop Pulls over Actual Cop",
+        "Florida Man Gets Trapped in Porta-potty, Busted for Drugs",
+        "Florida Man Driving Car Full of Stolen Mail Crashes into Trailer Full of Alpacas",
+      ];
+      final events = titles.map((title) => randomEvent(title: title));
+      final result = termFrequencies(events);
+      expect(
+          result,
+          equals({
+            "florida man breaks into joes crab shack, steals alcohol, leaves poop as payment":
+                {
+              "florida": 1 / 13,
+              "man": 1 / 13,
+              "breaks": 1 / 13,
+              "into": 1 / 13,
+              "joes": 1 / 13,
+              "crab": 1 / 13,
+              "shack": 1 / 13,
+              "steals": 1 / 13,
+              "alcohol": 1 / 13,
+              "leaves": 1 / 13,
+              "poop": 1 / 13,
+              "as": 1 / 13,
+              "payment": 1 / 13,
+            },
+            "florida man pretending to be cop pulls over actual cop": {
+              "florida": 1 / 10,
+              "man": 1 / 10,
+              "pretending": 1 / 10,
+              "to": 1 / 10,
+              "be": 1 / 10,
+              "cop": 2 / 10,
+              "pulls": 1 / 10,
+              "over": 1 / 10,
+              "actual": 1 / 10,
+            },
+            "florida man gets trapped in porta-potty, busted for drugs": {
+              "florida": 1 / 10,
+              "man": 1 / 10,
+              "gets": 1 / 10,
+              "trapped": 1 / 10,
+              "in": 1 / 10,
+              "porta": 1 / 10,
+              "potty": 1 / 10,
+              "busted": 1 / 10,
+              "for": 1 / 10,
+              "drugs": 1 / 10,
+            },
+            "florida man driving car full of stolen mail crashes into trailer full of alpacas":
+                {
+              "florida": 1 / 14,
+              "man": 1 / 14,
+              "driving": 1 / 14,
+              "car": 1 / 14,
+              "full": 2 / 14,
+              "of": 2 / 14,
+              "stolen": 1 / 14,
+              "mail": 1 / 14,
+              "crashes": 1 / 14,
+              "into": 1 / 14,
+              "trailer": 1 / 14,
+              "alpacas": 1 / 14,
+            },
+          }));
+    });
   });
 
   group("document frequencies", () {
     test("of an empty list of events are an empty map", () {
       const events = <Event>[];
-      final result = documentFrequencies(events);
+      final result = inverseDocumentFrequencies(events);
       expect(result, isEmpty);
     });
   });
